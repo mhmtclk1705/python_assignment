3b6583894dd5e3e9c4db32c0d4bb8354777384bd
mehmetcelik1705 <mehmetcelik1705@gmail.com>
2021-11-22 20:53:46 +0300

    Fibonacci_numbers colab den kopyalandı ve vim editörle kendi yerel repomda oluşturuldu ardından githuba push edildi

---
+++ Fibonacci_numbers.py
@@ -0,0 +1,20 @@
+# Task : Create a list consisting of Fibonacci numbers from 1 to 55 using control flow statements.
+
+# The desired output is like :
+
+# fibonacci →  [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+number_1 = 0
+number_2 = 1
+f_list = []
+count = 0
+f_numbers = 0
+while number_1 <= 55:
+  f_list.append(number_1)
+  f_numbers = number_1 + number_2
+  number_1 = number_2
+  number_2 = f_numbers
+  
+
+print(f_list)
+
+
